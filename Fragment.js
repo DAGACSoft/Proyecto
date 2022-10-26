6ec8b2b7cb1d151f9e312b007b29a3fa06414de4
CesarCordon <cesarmaurciciocordon@gmail.com>
2022-10-25 22:54:37 -0500

    Avance 25.10.2022

--- Fragment.js
+++ Fragment.js
@@ -8,6 +8,7 @@ export const App =() =>{
                 </head>
                 <body>
                     <h1>Este es el Reporte de Ventas</h1>
+                    <h1 className="Sub_Title">Venta por Mes</h1>
                     <div className="box_Title"> 
                         <h2>Venta por Unidades </h2>
                     </div>
@@ -15,21 +16,46 @@ export const App =() =>{
                         <h2>Venta $</h2>
                     </div>
                     <div className="box_Title">
-                        <h2>Venta mes</h2>
+                        <h2>Tipo de Consulta</h2>
                     </div>
-                    <div className="box_Data">
-                        <h2>{look_U()}</h2>
+                    <div className="box_VentaMes">
+                        <h2 id="Venta_U">0</h2>
                     </div>
-                    <div className="box_Data">
-                        <h2>{look_V()}</h2>
+                    <div className="box_VentaMes">
+                        <h2 id="Venta_V">0</h2>
                     </div>
-                    <div className="box_Data">
+                    <div className="box_VentaMes">
                         <p></p>
                         <select className="MES" id="MES_I">
                         <option value="VACIO" selected>Selecciones mes</option>
+                        <option value="ENERO">ENERO</option>
+                        <option value="FEBRERO">FEBRERO</option>
+                        <option value="MARZO">MARZO</option>
+                        <option value="ABRIL">ABRIL</option>
+                        <option value="MAYO">MAYO</option>
+                        <option value="JUNIO">JUNIO</option>
+                        <option value="JULIO">JULIO</option>
                         <option value="AGOSTO">AGOSTO</option>
                         <option value="SEPTIEMBRE">SEPTIEMBRE</option>
+                        <option value="OCTUBRE">OCTUBRE</option>
+                        <option value="NOVIEMBRE">NOVIEMBRE</option>
+                        <option value="DICIEMBRE">DICIEMBRE</option>
                         </select>
+                        <p></p>
+                        <button id="consultar" onClick={constultM}>Consulta</button>
+                    </div>
+                    <h1 className="Sub_Title">Venta por material</h1>
+                    <div className="box_VentaMes">
+                        <h2 id="Venta_U">0</h2>
+                    </div>
+                    <div className="box_VentaMes">
+                        <h2 id="Venta_V">0</h2>
+                    </div>
+                    <div className="box_VentaMes">
+                        <p></p>
+                        <input id="material"></input>
+                        <p></p>
+                        <button id="consultar" onClick={constultM}>Consulta</button>
                     </div>
                 </body>
             </html>
@@ -39,7 +65,7 @@ export const App =() =>{
 
 const Venta_mes =[
     {MES: 'AGOSTO', Unds: 88988989, Valor: 1000000},
-    {MES: 'SEPTIEMBRE',Unds: 100001, Valor: 1000001},
+    {MES: 'SEPTIEMBRE',Unds: 100001, Valor: 10067001},
     {MES: 'OCTUBRE',Unds: 103001, Valor: 1020001}
 ]
 
@@ -49,7 +75,7 @@ function look_U(Mes_id){
         return 0;
     }
     else{
-    return Venta_mes[K].Unds;}
+    return new Intl.NumberFormat('es-CO').format(Venta_mes[K].Unds);}
 }
 
 function look_V(Mes_id){
@@ -58,5 +84,12 @@ function look_V(Mes_id){
         return 0;
     }
     else{
-    return Venta_mes[K].Valor;}
+    return new Intl.NumberFormat('es-CO',{style: 'currency', currency:'COP', maximumFractionDigits: 0}).format(Venta_mes[K].Valor);}
+}
+
+function constultM (){
+    var Vu =  look_U(document.getElementById("MES_I").value);
+    document.getElementById("Venta_U").innerHTML = Vu;
+    var Vv =  look_V(document.getElementById("MES_I").value);
+    document.getElementById("Venta_V").innerHTML = Vv;
 }
